7505ace36b3de957cf88ffbeaae4efa90b2c74fd
Hidenori Miyoshi <miyohide@gmail.com>
2022-04-24 15:56:27 +0900

    create initial project files

---
+++ src/main/java/com/github/miyohide/appconfigsample/AppConfigSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.appconfigsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfigSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/miyohide/appconfigsample/AppConfigSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.appconfigsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppConfigSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
