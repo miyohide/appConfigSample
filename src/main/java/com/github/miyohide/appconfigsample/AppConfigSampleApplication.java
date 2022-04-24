9c87ff8fb774d50c2174f6c5eb12f300f56fc941
Hidenori Miyoshi <miyohide@gmail.com>
2022-04-24 20:25:00 +0900

    enable configuration properties

--- src/main/java/com/github/miyohide/appconfigsample/AppConfigSampleApplication.java
+++ src/main/java/com/github/miyohide/appconfigsample/AppConfigSampleApplication.java
@@ -2,8 +2,10 @@ package com.github.miyohide.appconfigsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MessageProperties.class)
 public class AppConfigSampleApplication {
 
     public static void main(String[] args) {
