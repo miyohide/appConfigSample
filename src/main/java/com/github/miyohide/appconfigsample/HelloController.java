c7e7560271ef5368bb41365a9b0667170970682b
Hidenori Miyoshi <miyohide@gmail.com>
2022-05-01 16:26:41 +0900

    add /welcome mapping

--- src/main/java/com/github/miyohide/appconfigsample/HelloController.java
+++ src/main/java/com/github/miyohide/appconfigsample/HelloController.java
@@ -1,18 +1,31 @@
 package com.github.miyohide.appconfigsample;
 
+import com.azure.spring.cloud.feature.manager.FeatureManager;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@ConfigurationProperties("controller")
 public class HelloController {
     private final MessageProperties properties;
+    private FeatureManager featureManager;
 
-    public HelloController(MessageProperties properties) {
+    public HelloController(MessageProperties properties, FeatureManager featureManager) {
         this.properties = properties;
+        this.featureManager = featureManager;
     }
 
     @GetMapping
     public String getMessage() {
         return "Message:" + properties.getMessage();
+    }
+
+    @GetMapping("/welcome")
+    public String mainWithParam(Model model) {
+        model.addAttribute("Beta", featureManager.isEnabledAsync("Beta").block());
+        return "welcome";
     }
 }
