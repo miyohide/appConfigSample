42f4cbb6dea15b254abc20efcc894f220d57fbd8
Hidenori Miyoshi <miyohide@gmail.com>
2022-05-02 18:09:06 +0900

    add comments

--- src/main/java/com/github/miyohide/appconfigsample/MessageProperties.java
+++ src/main/java/com/github/miyohide/appconfigsample/MessageProperties.java
@@ -4,6 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "config")
 public class MessageProperties {
+    /**
+     * ルートにアクセスしたときに出力するメッセージ
+     */
     private String message;
 
     public String getMessage() {
