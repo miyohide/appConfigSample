f0a2b8f299ee26abd619b6d5b54e718081b124e2
Hidenori Miyoshi <miyohide@gmail.com>
2022-05-03 20:48:45 +0900

    remove unused import statement

--- src/main/java/com/github/miyohide/appconfigsample/HelloController.java
+++ src/main/java/com/github/miyohide/appconfigsample/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @ConfigurationProperties("controller")
